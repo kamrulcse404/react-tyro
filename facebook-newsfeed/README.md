2f0e7147ae9bcd687e7fd899df72fd5e977600d2
kamrulcse404 <kamrul.h01960@gmail.com>
2022-10-07 12:14:10 +0600

    facebook newsfeed app added

---
+++ facebook-newsfeed/README.md
@@ -0,0 +1,1 @@
+This is my second react application facebook newsfeed
---
+++ facebook-newsfeed/src/components/post.component.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <h1>Hello Facebook Newsfeed App</h1>
+        );
+    }
+}
+ 
+export default Post;
