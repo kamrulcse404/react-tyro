d84e2c877856a25868fcf8c13f516208f3b82b64
kamrulcse404 <kamrul.h01960@gmail.com>
2022-10-12 02:37:49 +0600

    design fix

--- facebook-newsfeed/src/components/post.component.jsx
+++ facebook-newsfeed/src/components/post.component.jsx
@@ -1,12 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Post extends Component {
-    state = {  } 
-    render() { 
-        return (
-            <h1>Hello Facebook Newsfeed App</h1>
-        );
+  state = {
+    post: {
+        imgSrc: './../../images/kamrul.jpg',
+        userName: 'Kamrul Hasan',
+        postDuration: '1h',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, nam.',
+        isLiked: false
     }
+  };
+  render() {
+    const {imgSrc, userName, postDuration, content, isLiked} = this.state.post;
+    return (
+      <div className="container max-auto">
+        <div className="post-wrapper">
+          <div className="post aling-items-center d-flex">
+            <div className="image-wrapper">
+              <img src={ imgSrc } />
+            </div>
+            <div className="d-flex flex-column">
+              <h2>{ userName }</h2>
+              <div>{ postDuration }</div>
+            </div>
+          </div>
+          <div className="post-content">
+            <p>{ content }</p>
+          </div>
+          <div className="social d-flex justify-content-between">
+            <div>{ isLiked ? 'Liked' : 'Like' }</div>
+            <div>Comment</div>
+            <div>Share</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Post;
--- facebook-newsfeed/src/index.js
+++ facebook-newsfeed/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Post from './components/post.component';
 import "bootstrap/dist/css/bootstrap.css";
+import './post.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
