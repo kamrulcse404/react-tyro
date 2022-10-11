a0d0ccc716011383fbc4e4146ed0f3a958d46ed9
kamrulcse404 <kamrul.h01960@gmail.com>
2022-10-12 03:26:21 +0600

    design updated

--- facebook-newsfeed/src/components/post.component.jsx
+++ facebook-newsfeed/src/components/post.component.jsx
@@ -3,32 +3,35 @@ import React, { Component } from "react";
 class Post extends Component {
   state = {
     post: {
-        imgSrc: './../../images/kamrul.jpg',
-        userName: 'Kamrul Hasan',
-        postDuration: '1h',
-        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, nam.',
-        isLiked: false
-    }
+      imgSrc: "./../../images/kamrul.jpg",
+      userName: "Kamrul Hasan",
+      postDuration: "1h",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, nam.",
+      isLiked: false,
+    },
   };
   render() {
-    const {imgSrc, userName, postDuration, content, isLiked} = this.state.post;
+    const { imgSrc, userName, postDuration, content, isLiked } =
+      this.state.post;
     return (
-      <div className="container max-auto">
+      <div className="container">
         <div className="post-wrapper">
           <div className="post aling-items-center d-flex">
             <div className="image-wrapper">
-              <img src={ imgSrc } />
+              <img src={imgSrc} />
             </div>
             <div className="d-flex flex-column">
-              <h2>{ userName }</h2>
-              <div>{ postDuration }</div>
+              <h2>{userName}</h2>
+              <div>{postDuration}</div>
             </div>
           </div>
+          {/* <div className="settings">...</div> */}
           <div className="post-content">
-            <p>{ content }</p>
+            <p>{content}</p>
           </div>
           <div className="social d-flex justify-content-between">
-            <div>{ isLiked ? 'Liked' : 'Like' }</div>
+            <div>{isLiked ? "Liked" : "Like"}</div>
             <div>Comment</div>
             <div>Share</div>
           </div>
